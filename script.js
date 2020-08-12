0931df83d4388f783506fcaeb2539e0f5aa01d47
dawa93 <dawa93@naver.com>
2020-08-12 19:32:51 +0900

    Completed making new feeds

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 // your code here
+for (let i = 0; i < DATA.length; i++) {
+  const feed = document.querySelector("#feed");
+  let newfeed = document.createElement("div");
+  let nickname = document.createElement("span");
+  let date = document.createElement("span");
+  let usercomment = document.createElement("div");
+  let line = document.createElement("div");
+
+  line.classList.add("line");
+  newfeed.classList.add("newfeed");
+  nickname.classList.add("nickname");
+  date.classList.add("date");
+  usercomment.classList.add("user_comment");
+
+  feed.append(newfeed);
+  nickname.textContent = DATA[i].user;
+  date.textContent = DATA[i].created_at;
+  usercomment.textContent = DATA[i].message;
+
+  newfeed.append(nickname);
+  newfeed.append(date);
+  newfeed.append(usercomment);
+  newfeed.append(line);
+}
 
 // DATA는 이미 작성된 트윗을 표시합니다.
-console.log(DATA)
+console.log(DATA);
 
 // generateNewTweet을 호출할 때마다 새로운 트윗을 생성합니다.
 console.log(generateNewTweet());
 
-document.getElementById('test').innerHTML = 'hello twittler, check developer console!';
+// document.getElementById("test").innerHTML =
+("hello twittler, check developer console!");
